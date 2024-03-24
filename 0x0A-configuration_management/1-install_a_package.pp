6aad8e2341ac293b5b492de6f6a8ebc766dcc5d8
Salsabil44 <achisalsabil556@gmail.com>
2024-03-23 19:29:27 -0700

    killme

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
+#Install Werkzeug version 2.1.1
+package { 'Werkzeug':
+ensure => '2.1.1',
+provider => 'pip3',
+}
 #Install flask version 2.1.0
-
 exec {'pip3 install flask':
 require => Exec['python-installed'],
 command => '/usr/bin/pip3 install flask==2.1.0'
